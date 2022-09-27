37762b23306b7eec2441e34bc9083aa74d72ff16
Alexander Kim <64238640+aksovius@users.noreply.github.com>
2022-09-27 20:04:39 +0900

    front

---
+++ client/src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Spinner() {
+  return (
+    <div className="d-flex justify-context-center">
+        <div className="spinner-border" role="status">
+            <span className="sr-only"></span>
+        </div>
+    </div>
+  )
+}
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const colors = require('colors');
+const cors = require('cors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
@@ -10,7 +11,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 // Connect to the database
 connectDB() 
-
+app.use(cors());
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === 'development'
